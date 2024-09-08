39c2e884df1e19beb98ee2d199aea99290514e99
ibnsalah <ahmed.a.salah24@gmail.com>
2024-09-08 21:14:33 +0300

    حفظ التغييرات المحلية قبل الدمج

--- routes/web.php
+++ routes/web.php
@@ -67,13 +67,13 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');;
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index')->middleware('auth');;
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware('auth');;
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware('auth');;
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit')->middleware('auth');;
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');;
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');;
 
 Route::get('/single', [ProductController::class, 'single'])->name('single');
 

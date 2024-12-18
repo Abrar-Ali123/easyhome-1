bfcaac304c4f6b15ca0485238041843bb81062c7
Abrar-Ali123 <82758512+Abrar-Ali123@users.noreply.github.com>
2024-12-18 18:35:09 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,7 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
-<head>
-<link rel="icon" href="{{ asset('/images/9.png') }}">
+@include('parts.head')
 
-
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PWNWC86L');</script>
-
-<meta charset="utf-8">
-
-      <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-      <script src="{{ asset('js/site.js') }}"></script>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-     <!-- Fonts -->
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>  easyhome</title>
-     </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWNWC86L"
---
+++ resources/views/parts/head.blade.php
@@ -0,0 +1,52 @@
+<head>
+    <meta charset="utf-8">
+    <title>DreamHome - Real Estate HTML Template</title>
+
+    <meta name="author" content="themesflat.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/JhBX9KZ7bXFOjT5x4bxGgAK3EBkIqzMeqK6F2F7Hz4abFTpXNolPqSHRAcAwdsjCHD8u1J9Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+   <link href="{{ asset('css/dist/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dist/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dist/responsive.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dist/owl.css') }}" rel="stylesheet">
+
+
+
+    <!-- Javascript -->
+    <script src="{{ asset('css/js/jquery.min.js') }}"></script>
+
+<script src="{{ asset('css/js/jquery.easing.js') }}"></script>
+
+<script src="{{ asset('css/js/jquery.nice-select.min.js') }}"></script>
+
+<script src="{{ asset('css/js/bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('css/js/swiper-bundle.min.js') }}"></script>
+
+<script src="{{ asset('css/js/owl.js') }}"></script>
+
+<script src="{{ asset('css/js/swiper.js') }}"></script>
+
+<script src="{{ asset('css/js/price-ranger.js') }}"></script>
+
+<script src="{{ asset('css/js/curved.js') }}"></script>
+
+<script src="{{ asset('css/js/main.js') }}"></script>
+
+<script src="{{ asset('css/js/shortcodes.js') }}"></script>
+
+<script src="{{ asset('css/js/plugin.js') }}"></script>
+
+<script src="{{ asset('css/js/countto.js') }}"></script>
+
+<script src="{{ asset('css/js/jquery-validate.js') }}"></script>
+
+    <!-- Favicon and Touch Icons  -->
+    <link rel="shortcut icon" href="assets/images/logo/Favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/logo/Favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
